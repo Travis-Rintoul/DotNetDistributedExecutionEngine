de0c586c356fd1b2ddb2ca539c8d91129a64a1d4
Travis Rintoul <travis.rintoul@gmail.com>
2026-03-08 06:20:41 +1000

    added Basic job example

---
+++ DistributedExecutionEngine.Application/Jobs/Handlers/BasicJobHandler.cs
@@ -0,0 +1,16 @@
+using DistributedExecutionEngine.Application.Orchestrator;
+
+namespace DistributedExecutionEngine.Application.Jobs.Handlers;
+
+public sealed class BasicJob
+{
+    public string Foo { get; set; } = string.Empty;
+}
+
+public sealed class BasicJobHandler : IJobHandler<BasicJob>
+{
+    public Task<JobResult> ExecuteAsync(BasicJob job, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(new JobResult { Message = "Hello!" });
+    }
+}
