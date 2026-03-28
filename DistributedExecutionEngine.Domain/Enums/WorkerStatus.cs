8dd66fc64c6634c6a98cf4998123673a01c420bd
Travis Rintoul <travis.rintoul@gmail.com>
2026-03-28 23:28:13 +1000

    Updated WorkerStatus enum

--- DistributedExecutionEngine.Domain/Enums/WorkerStatus.cs
+++ DistributedExecutionEngine.Domain/Enums/WorkerStatus.cs
@@ -2,6 +2,8 @@ namespace DistributedExecutionEngine.Library.Application.Worker;
 
 public enum WorkerStatus
 {
-    Waiting,
-    Running,
+    Pending = 0,
+    Starting = 1,
+    Running = 2,
+    Failed = 3
 }
