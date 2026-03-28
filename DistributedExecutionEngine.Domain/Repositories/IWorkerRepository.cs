a5603254dce96d28bbddac49973065c2cdc1c8f9
Travis Rintoul <travis.rintoul@gmail.com>
2026-03-28 23:29:28 +1000

    Added ClaimPendingWorkerAsync method to WorkerRepository

--- DistributedExecutionEngine.Domain/Repositories/IWorkerRepository.cs
+++ DistributedExecutionEngine.Domain/Repositories/IWorkerRepository.cs
@@ -5,6 +5,7 @@ namespace DistributedExecutionEngine.Domain.Repositories;
 public interface IWorkerRepository
 {
     public Task<int> RegisterWorkerAsync(Worker worker);
+    public Task<Worker?> ClaimPendingWorkerAsync();
     public Task UpdateHeartbeat(Worker worker);
     public Task<int> Count();
 }
--- DistributedExecutionEngine.Infrastructure/Repositories/WorkerRepository.cs
+++ DistributedExecutionEngine.Infrastructure/Repositories/WorkerRepository.cs
@@ -1,6 +1,8 @@
 using DistributedExecutionEngine.Domain.Entities;
 using DistributedExecutionEngine.Domain.Repositories;
 using DistributedExecutionEngine.Infrastructure.Persistence;
+using DistributedExecutionEngine.Library.Application.Worker;
+using Microsoft.EntityFrameworkCore;
 
 namespace DistributedExecutionEngine.Infrastructure.Repositories;
 
@@ -11,6 +13,30 @@ public sealed class WorkerRepository(OrchestratorDbContext context) : IWorkerRep
         await context.Workers.AddAsync(worker);
         await context.SaveChangesAsync();
         return worker.Id;
+    }
+
+    public async Task<Worker?> ClaimPendingWorkerAsync()
+    {
+        const int starting = (int)WorkerStatus.Starting;
+        const int pending = (int)WorkerStatus.Pending;
+
+        var results = await context.Workers
+            .FromSqlRaw("""
+                UPDATE "Workers"
+                SET "Status" = {0}
+                WHERE "Id" = (
+                    SELECT "Id" FROM "Workers"
+                    WHERE "Status" = {1}
+                    ORDER BY "Id"
+                    LIMIT 1
+                    FOR UPDATE SKIP LOCKED
+                )
+                RETURNING *;
+            """, starting, pending)
+            .AsNoTracking()
+            .ToListAsync();
+
+        return results.SingleOrDefault();
     }
 
     public async Task UpdateHeartbeat(Worker worker)
