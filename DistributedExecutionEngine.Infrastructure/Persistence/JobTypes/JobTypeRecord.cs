9bd4a0eb8b9de9f8809b1141a3b5653e96e4caaf
Travis Rintoul <travis.rintoul@gmail.com>
2026-06-02 17:14:46 +1000

    Updated JobTypeRecord and config

--- DistributedExecutionEngine.Infrastructure/Persistence/JobTypes/JobTypeRecord.cs
+++ DistributedExecutionEngine.Infrastructure/Persistence/JobTypes/JobTypeRecord.cs
@@ -1,10 +1,10 @@
-namespace DistributedExecutionEngine.Infrastructure.Persistence.Records;
+namespace DistributedExecutionEngine.Infrastructure.Persistence.JobTypes;
 
 public class JobTypeRecord
 {
     public long Id { get; set; }
-    public string Key { get; set; } = null!;
-    public string DisplayName { get; set; } = null!;
+    public string Code { get; set; } = null!;
+    public string DisplayName { get; set; } = string.Empty;
     public bool IsEnabled { get; set; }
     public int DefaultMaxAttempts { get; set; }
     public int? DefaultTimeoutSeconds { get; set; }
--- DistributedExecutionEngine.Infrastructure/Persistence/JobTypes/JobTypeRecordConfiguration.cs
+++ DistributedExecutionEngine.Infrastructure/Persistence/JobTypes/JobTypeRecordConfiguration.cs
@@ -1,20 +1,20 @@
-using DistributedExecutionEngine.Infrastructure.Persistence.Records;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace DistributedExecutionEngine.Infrastructure.Persistence.JobType;
+namespace DistributedExecutionEngine.Infrastructure.Persistence.JobTypes;
 
 public class JobTypeRecordConfiguration : IEntityTypeConfiguration<JobTypeRecord>
 {
     public void Configure(EntityTypeBuilder<JobTypeRecord> builder)
     {
         builder.HasKey(x => x.Id);
+        builder.HasAlternateKey(x => x.Code);
         
-        builder.Property(x => x.Key)
+        builder.Property(x => x.Code)
             .HasMaxLength(200)
             .IsRequired();
 
-        builder.HasIndex(x => x.Key)
+        builder.HasIndex(x => x.Code)
             .IsUnique();
     }
 }
