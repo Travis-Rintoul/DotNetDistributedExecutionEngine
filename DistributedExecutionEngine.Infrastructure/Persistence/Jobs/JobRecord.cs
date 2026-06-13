494c66d797fb4485ecbae46e4d87105039b9f9ce
Travis Rintoul <travis.rintoul@gmail.com>
2026-06-13 19:07:57 +1000

    updated JobRecord persistence model to define fiels per an interface for cleaner access later.

--- DistributedExecutionEngine.Infrastructure/Persistence/Jobs/JobRecord.cs
+++ DistributedExecutionEngine.Infrastructure/Persistence/Jobs/JobRecord.cs
@@ -1,20 +1,32 @@
 using DistributedExecutionEngine.Domain.Aggregates.Jobs;
 using DistributedExecutionEngine.Domain.Aggregates.JobTypes;
+using DistributedExecutionEngine.Infrastructure.Persistence.Jobs.JobLeasing;
+using DistributedExecutionEngine.Infrastructure.Persistence.Jobs.JobStatuses;
 
 namespace DistributedExecutionEngine.Infrastructure.Persistence.Jobs;
 
-public sealed class JobRecord
+public sealed record JobRecord : IJobStatusRecord, IJobLeaseRecord
 {
+    // Main fields
     public long Id { get; set; }
     public Guid JobId { get; set; }
-    public JobStatusCode Status { get; set; }
     public string? PayloadJson { get; set; }
     public string JobTypeCode { get; set; }
-    public DateTimeOffset CreatedUtc { get; private set; }
-    public DateTimeOffset? StartedUtc { get; private set; }
-    public DateTimeOffset? CompletedUtc { get; private set; }
-    public DateTimeOffset? LeasedUtc { get; set; }
-    public int? AssignedWorkerId { get; set; }
     public int AttemptsCount { get; set; }
     public int MaxAttemptsCount { get; set; }
+    
+    // Status fields
+    public JobStatusCode StatusCode { get; set; }
+    public DateTimeOffset CreatedUtc { get; set; }
+    public DateTimeOffset? StartedUtc { get; set; }
+    public DateTimeOffset? CompletedUtc { get; set; }
+    public DateTimeOffset? FailedUtc { get; set; }
+    public string? FailureReason { get; set; }
+    
+    // Lease fields
+    public JobLeaseStatusCode LeaseStatusCode { get; set; }
+    public DateTimeOffset? LeasedUtc { get; set; }
+    public DateTimeOffset? LeaseExpirationUtc { get; set; }
+    public Guid? AssignedWorkerId { get; set; }
+    
 }
