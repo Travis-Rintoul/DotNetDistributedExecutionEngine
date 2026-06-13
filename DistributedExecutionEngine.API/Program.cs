54ed18c9e3618c5beedf3579c828b985adf8d37c
Travis Rintoul <travis.rintoul@gmail.com>
2026-06-13 19:25:17 +1000

    implemented mininal api endpoints.

--- DistributedExecutionEngine.API/Program.cs
+++ DistributedExecutionEngine.API/Program.cs
@@ -1,6 +1,9 @@
 using DistributedExecutionEngine.Application;
 using DistributedExecutionEngine.Application.Abstractions.Messaging;
+using DistributedExecutionEngine.Application.Features.Jobs.Queries;
 using DistributedExecutionEngine.Application.Features.Jobs.Scheduling;
+using DistributedExecutionEngine.Application.Features.JobTypes.Queries;
+using DistributedExecutionEngine.Application.Features.Workers.Queries;
 using DistributedExecutionEngine.Domain.Aggregates.Jobs;
 using DistributedExecutionEngine.Domain.Common;
 using DistributedExecutionEngine.Infrastructure;
@@ -15,9 +18,22 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 
 app.UseHttpsRedirection();
-app.MapPost("/jobs", async (ICommandHandler<ScheduleJobCommand, Result<JobId, ScheduleJobError>> jobCommandHandler) =>
+app.MapPost("/jobs", async (ICommandDispatcher dispatcher) =>
 {
-    await jobCommandHandler.HandleAsync(new ScheduleJobCommand(Job.Create("GENERIC-JOB", null)));
+    return (await dispatcher.SendAsync(new ScheduleJobCommand(Job.Create("GENERIC-JOB", null))))
+        .Match(
+            success => Results.Ok(success.Value),
+            Results.BadRequest
+        );
 });
+
+app.MapGet("/jobs", async (IQueryDispatcher dispatcher) =>
+    Results.Ok(await dispatcher.SendAsync(new GetJobsQuery())));
+
+app.MapGet("/workers", async (IQueryDispatcher dispatcher) =>
+    Results.Ok(await dispatcher.SendAsync(new GetWorkersQuery())));
+
+app.MapGet("/job-types", async (IQueryDispatcher dispatcher) =>
+    Results.Ok(await dispatcher.SendAsync(new GetJobTypesQuery())));
 
 app.Run();
